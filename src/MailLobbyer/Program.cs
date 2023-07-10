742d161cbec1d056930ef73db46fdbb65d1d61c5
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2023-07-10 16:29:58 +0100

    Update

--- src/MailLobbyer/Program.cs
+++ src/MailLobbyer/Program.cs
@@ -28,28 +28,6 @@ app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-//Checks for a folder for CSV files for contact groups in easy to access locations on both Linux and Windows, if it does not exist a folder is created.
-string documentspath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-string directoryname = "MailLobbyer";
-string directorypath = Path.Combine(documentspath, directoryname);
-
-if (!Directory.Exists(directorypath))
-{
-    Directory.CreateDirectory(directorypath);
-    Console.WriteLine("CSV contact-grouping directory created successfully.");
-}
-else
-{
-    Console.WriteLine("CSV contact-grouping directory already exists.");
-}
-
-CSVService csvserviceinstance = new CSVService();
-csvserviceinstance.CSVFileSeeker(directorypath);
-
-foreach (CSVFile csvfile in csvserviceinstance.CSVFilesindir)
-{
-    await csvserviceinstance.CSVParser(csvfile.Filepath);
-}
 
 app.Run();
 
