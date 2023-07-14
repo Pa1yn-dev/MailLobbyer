25569f74a510281a7ea099629e620c3a3b89b229
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2023-07-14 20:38:51 +0100

    Update: File attachments
    Needs some refactoring, in terms of EmailHandler class methods being called individually.

--- src/MailLobbyer/modules/EmailHandler.cs
+++ src/MailLobbyer/modules/EmailHandler.cs
@@ -4,6 +4,8 @@ using MimeKit;
 using System.Threading.Tasks;
 using MailLobbyer.SmtpClientSettingsComponent;
 using MailLobbyer.ContactClass;
+using Microsoft.AspNetCore.Components.Forms;
+using MailLobbyer.FileUploadClass;
 
 namespace MailLobbyer.EmailHandlerComponent
 {
@@ -11,13 +13,28 @@ namespace MailLobbyer.EmailHandlerComponent
     {
 
         private readonly SmtpClientSettings _smtpSettings;
+        public List<FileUpload> fileuploads = new List<FileUpload>();
         public EmailHandler(IConfiguration configuration)
         {
             _smtpSettings = new SmtpClientSettings();
             configuration.GetSection("SmtpClientSettings").Bind(_smtpSettings);
         }
 
-        public async Task EmailSyntaxHandler(string subject, string body, string exclude, Contact contact)
+        public async Task ExtractUploadedFileContents(List<IBrowserFile> selectedfiles)
+        {
+            if(selectedfiles.Count > 0)
+            {
+                foreach (var file in selectedfiles)
+                {
+                    byte[] filecontents = new byte[file.Size];
+                    await file.OpenReadStream().ReadAsync(filecontents);
+                    FileUpload fileupload = new FileUpload(file.Name, file.Size, filecontents);
+                    fileuploads.Add(fileupload);
+                }
+            }
+        }
+
+        public async Task EmailSyntaxHandler(string subject, string body, string exclude, Contact contact, List<FileUpload> fileUploads)
         {
             string prefixsyntax = "/P";
             string fullnamesyntax = "/FN";
@@ -34,16 +51,30 @@ namespace MailLobbyer.EmailHandlerComponent
                             .Replace(forenamesyntax, contact.Forename, StringComparison.OrdinalIgnoreCase)
                             .Replace(surnamesyntax, contact.Surname, StringComparison.OrdinalIgnoreCase);
             
-            await SendEmailAsync(string.Concat(contact.Forename, " ", contact.Surname),contact.Email,subject,body);
+            await SendEmailAsync(string.Concat(contact.Forename, " ", contact.Surname),contact.Email,subject,body,fileUploads);
         }
 
-        public async Task SendEmailAsync(string RecipientName, string RecipientEmail, string subject, string body)
+        public async Task SendEmailAsync(string RecipientName, string RecipientEmail, string subject, string body, List<FileUpload> fileuploads)
         {
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress(_smtpSettings.Sendername, _smtpSettings.Senderemail));
             message.To.Add(new MailboxAddress(RecipientName, RecipientEmail));
             message.Subject = subject;
-            message.Body = new TextPart("plain") { Text = body };
+
+            var builder = new BodyBuilder();
+
+            builder.TextBody = body;
+
+            if(fileuploads.Any())
+            {
+                foreach (FileUpload fileupload in fileuploads)
+                {
+                    builder.Attachments.Add(fileupload.Filename, fileupload.Filecontents);
+                }
+            }
+
+            message.Body = builder.ToMessageBody();
+            //message.Body = new TextPart("plain") { Text = body };
 
             using (var client = new SmtpClient())
             {
