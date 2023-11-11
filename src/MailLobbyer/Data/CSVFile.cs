3065e44eb201050ec5979f43cd9f940ae6c768a8
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:34:42 +0000

    Added ability to add and remove CSV files from disk and tweaked memory storage.
    Changes:
    - CSV files now stored on memory to ensure that in the case another user deletes a CSV file when another is emailing it is safe.
    - Add and remove CSV files from browser, you can still upload to server directory from the server.
    
    Note: Currently CSV files with the same name will overwrite ones on disk with the same name, I will address this when I get more time.

--- src/MailLobbyer/Data/CSVFile.cs
+++ src/MailLobbyer/Data/CSVFile.cs
@@ -2,13 +2,17 @@ namespace MailLobbyer.CSVFileClass
 {
     public class CSVFile
     {
+        public Guid Id { get; }
         public string Filename { get; set; }
         public string Filepath { get; set; }
+        public byte[] Filecontents { get; set; }
 
-        public CSVFile (string filename, string filepath)
+        public CSVFile (Guid id, string filename, string filepath, byte[] filecontents)
         {
+            Id = id;
             Filename = filename;
             Filepath = filepath;
+            Filecontents = filecontents;
         }
     }
 }
--- src/MailLobbyer/Hubs/CSVHub.cs
+++ src/MailLobbyer/Hubs/CSVHub.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.SignalR;
 using MailLobbyer.CSVFileClass;
 using MailLobbyer.ContactClass;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace MailLobbyer.Server.Hubs;
 
 public class CSVHub : Hub
 {
     private static List<CSVFile> csvfilesinmemory = new List<CSVFile>();
+    private static string[] filepaths;
 
     public async Task CSVFileSeeker()
     {
@@ -24,14 +26,50 @@ public class CSVHub : Hub
         }
 
         // Later on change to use config values set by the user
-        string[] filepaths = Directory.GetFiles(directorypath);
+        filepaths = Directory.GetFiles(directorypath);
 
         foreach (string filepath in filepaths)
         {
-            CSVFile newcsvfile = new CSVFile(Path.GetFileNameWithoutExtension(filepath), filepath);
-            csvfilesinmemory.Add(newcsvfile);
-            System.Console.WriteLine(newcsvfile.Filename);
+
+            using (var ms = new MemoryStream())
+            {
+                using(FileStream fs = new FileStream(filepath, FileMode.Open,FileAccess.Read))
+                {
+                    await fs.CopyToAsync(ms);
+                }
+
+                byte[] filecontents = ms.ToArray();
+                CSVFile newcsvfile = new CSVFile(Guid.NewGuid(),Path.GetFileNameWithoutExtension(filepath), filepath, filecontents);
+                csvfilesinmemory.Add(newcsvfile);
+            }
+            
         }
+    }
+
+    public async Task CSVFilesToUpload(IBrowserFile file)
+    {
+        const long MaxAllowedFileSize = 26214400;
+        string directorypath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "MailLobbyer");
+
+
+
+        using (FileStream fs = new FileStream(directorypath, FileMode.Create))
+        {
+            await file.OpenReadStream(MaxAllowedFileSize).CopyToAsync(fs);
+        }
+        
+    }
+
+    public async Task RemoveCSVFile(string selectedcsvfilename)
+    {
+        foreach (string filepath in filepaths)
+        {
+            if(Path.GetFileNameWithoutExtension(filepath) == selectedcsvfilename)
+            {
+                File.Delete(filepath);
+            }
+        }
+        
     }
 
 
