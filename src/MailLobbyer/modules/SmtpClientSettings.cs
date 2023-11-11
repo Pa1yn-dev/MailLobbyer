bf8e595e0552bbba512550f935b1a8b9947b6556
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:34:17 +0000

    Update using Iconfiguration

--- src/MailLobbyer/modules/SmtpClientSettings.cs
+++ src/MailLobbyer/modules/SmtpClientSettings.cs
@@ -1,7 +1,9 @@
-namespace MailLobbyer.SmtpClientSettings
+namespace MailLobbyer.SmtpClientSettingsComponent
 {
     public class SmtpClientSettings
     {
+        public string? Sendername { get; set; }
+        public string? Senderemail { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? Host { get; set; }
