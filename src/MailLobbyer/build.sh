ea64080e07e86203e0d4dae2d3a2291b40f97959
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:34:42 +0000

    Update build.sh

--- src/MailLobbyer/build.sh
+++ src/MailLobbyer/build.sh
@@ -11,3 +11,9 @@ dotnet publish -c Release -r linux-x64 --self-contained true -o ./builds/ML-linu
 
 # Zip the Linux version
 zip -r ./builds/ML-linux-x64.zip ./builds/ML-linux-x64
+
+# Publish for OSX
+dotnet publish -c Release -r osx-x64 --self-contained true -o ./builds/ML-osx-x64
+
+# Zip the OSX version
+zip -r ./builds/ML-osx-x64.zip ./builds/ML-osx-x64
