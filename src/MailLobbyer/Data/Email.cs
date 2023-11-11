e08353b9e1be14843d28488953b77d697ae0a6a6
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:34:41 +0000

    Full server side rewrite

--- src/MailLobbyer/Data/Email.cs
+++ src/MailLobbyer/Data/Email.cs
@@ -1,5 +1,4 @@
-using MailLobbyer.ContactClass;
-using MailLobbyer.FileUploadClass;
+using MailLobbyer.SettingsProfilesClass;
 using MimeKit;
 
 namespace MailLobbyer.EmailClass;
@@ -7,9 +6,11 @@ namespace MailLobbyer.EmailClass;
     public class Email 
     {
         public MimeMessage ProcessedEmailContents { get; set; }
+        public SettingsProfiles SelectedProfile { get; set; }
 
-        public Email(MimeMessage processedemailcontents)
+        public Email(MimeMessage processedemailcontents, SettingsProfiles selectedprofile)
         {
             ProcessedEmailContents = processedemailcontents;
+            SelectedProfile = selectedprofile;
         }
     }
