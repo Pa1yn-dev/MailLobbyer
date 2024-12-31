f276833605c1c2caadbd4f7c3da1a6aef043c39a
Peter Hamilton <sgphamil@liverpool.ac.uk>
2024-12-31 20:59:11 +0000

    Update build.sh

--- src/MailLobbyer/build.sh
+++ src/MailLobbyer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Publish for Linux
-dotnet publish --os linux --output ./builds
-zip -r publish_linux.zip ./builds
+dotnet publish --os linux --output ./builds/linux
+zip -r ML-Linux-v1.2.4-Server.zip ./builds/linux
